36983a9684e86f8a22a2a3d39e39a0b8ded1d9fd
Hong Dung Nguyen <hongdung6992@gmail.com>
2019-12-04 21:31:54 +0700

    setting google map

--- app/components/Main/Maps/Maps.js
+++ app/components/Main/Maps/Maps.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 class Maps extends Component {
   static navigationOptions = {
     header: null,
   };
-  
+
   render() {
     return (
-      <View>
-        <Text>Map Component</Text>
+      <View style={{flex: 1}}>
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          region={{
+            latitude: 16.07633,
+            longitude: 108.21969,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default Maps;
