ab65ac51a05adb22b3d45a066c19b37b3a8920e4
Hong Dung <hongdung6992@gmail.com>
2020-02-26 17:03:45 +0700

    login react-native-community     /     google-signin

--- app/components/App.js
+++ app/components/App.js
@@ -10,6 +10,7 @@ import {
   GraphRequest,
   GraphRequestManager,
 } from 'react-native-fbsdk';
+import {GoogleSignin} from '@react-native-community/google-signin';
 import globals from './globals';
 
 class WrappedStack extends Component {
@@ -26,9 +27,29 @@ const ReloadAppOnLanguageChange = withNamespaces('common', {
 })(createAppContainer(WrappedStack));
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    GoogleSignin.configure();
+  }
+
   async componentDidMount() {
     SplashScreen.hide(); // hide splash screen when open app
     firebase.messaging().subscribeToTopic('all'); // Receive notification multi device
+
+    this.isSignedInFacebook();
+    this.isSignedInGoogle();
+
+    PushNotification.configure({
+      onNotification: function(notification) {
+        console.log('NOTIFICATION:', notification);
+      },
+    });
+  }
+
+  /**
+  Checking SignedIn Status with react-native-fbsdk
+ */
+  isSignedInFacebook = () => {
     AccessToken.getCurrentAccessToken()
       .then(user => {
         return user;
@@ -36,9 +57,9 @@ class App extends Component {
       .then(user => {
         const responseInfoCallback = (error, response) => {
           if (error) {
-            globals.onSignIn(null);
+            globals.onSignIn(null, false);
           } else {
-            globals.onSignIn(response);
+            globals.onSignIn(response, true);
           }
         };
         if (user) {
@@ -58,12 +79,36 @@ class App extends Component {
           new GraphRequestManager().addRequest(infoRequest).start();
         }
       });
-    PushNotification.configure({
-      onNotification: function(notification) {
-        console.log('NOTIFICATION:', notification);
-      },
-    });
-  }
+  };
+
+  /**
+  Checking SignedIn Status with google-signin
+ */
+  isSignedInGoogle = async () => {
+    const isSignedIn = await GoogleSignin.isSignedIn();
+    if (isSignedIn) {
+      this.getCurrentUserInfo()
+        .then(response => {
+          globals.onSignIn(response.user, isSignedIn, true);
+        })
+        .catch(() => {
+          globals.onSignIn(null, !isSignedIn, false);
+        });
+    }
+  };
+
+  /**
+    Get user info is signed in with google
+    @return object
+   */
+  getCurrentUserInfo = async () => {
+    try {
+      const response = await GoogleSignin.signInSilently();
+      return response;
+    } catch (error) {
+      return null;
+    }
+  };
 
   render() {
     return <ReloadAppOnLanguageChange />;
--- app/components/Main/Menu/Menu.js
+++ app/components/Main/Menu/Menu.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Alert,
+} from 'react-native';
 import {withNamespaces} from 'react-i18next';
-import {Icon} from 'react-native-elements';
+import {Icon, Avatar} from 'react-native-elements';
 import LinearGradient from 'react-native-linear-gradient';
 import AsyncStorage from '@react-native-community/async-storage';
 import i18n from '../../../utils/i18n';
@@ -22,15 +29,22 @@ class Menu extends Component {
     super(props);
     this.state = {
       languageCode: 'ja',
+      isSignedInGoogle: false,
+      isSignedIn: false,
       user: null,
     };
+    this.googleSignInConfigure();
     globals.onSignIn = this.onSignIn.bind(this);
   }
 
-  onSignIn = user => {
-    this.setState({user});
+  onSignIn = (user, isSignedIn, isSignedInGoogle) => {
+    this.setState({user, isSignedIn, isSignedInGoogle});
   };
 
+  /**
+    Change language and save language code to AsyncStorage
+    @param string
+   */
   async onChangeLanguage(lang) {
     i18n.changeLanguage(lang);
     try {
@@ -61,13 +75,22 @@ class Menu extends Component {
         }),
       )
       .catch(err => console.log(err));
+  }
+
+  /**
+    It is mandatory to call this method before attempting to call signIn() and signInSilently()
+ */
+  googleSignInConfigure = () => {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'],
       webClientId:
         '831086411403-vl8s60r47p5fca9a196qm493p3ua5ttl.apps.googleusercontent.com',
     });
-  }
+  };
 
+  /**
+  Handle login with facebook
+  */
   loginFacebook = async () => {
     LoginManager.logInWithPermissions(['public_profile'])
       .then(result => {
@@ -81,9 +104,9 @@ class Menu extends Component {
             .then(user => {
               const responseInfoCallback = (error, response) => {
                 if (error) {
-                  this.setState({user: null});
+                  this.setState({user: null, isSignedIn: false});
                 } else {
-                  this.setState({user: response});
+                  this.setState({user: response, isSignedIn: true});
                 }
               };
 
@@ -106,33 +129,113 @@ class Menu extends Component {
         }
       })
       .catch(error => {
-        console.log('error', error);
+        this.showSignInError(JSON.stringify(error));
       });
   };
 
+  /**
+    Handle login with facebook
+   */
   loginGoogle = async () => {
     try {
       await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      console.log(userInfo);
-      this.setState({user: userInfo.user});
+      const response = await GoogleSignin.signIn();
+      this.setState({
+        user: response.user,
+        isSignedIn: true,
+        isSignedInGoogle: true,
+      });
     } catch (error) {
-      console.log(error);
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        console.log('SIGN_IN_CANCELLED');
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log('IN_PROGRESS');
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log('PLAY_SERVICES_NOT_AVAILABLE');
-      } else {
-        console.log('Other');
-      }
+      this.handleSignInError(error);
     }
   };
 
-  logout = () => {
-    LoginManager.logOut();
-    this.setState({user: null});
+  /**
+    handle error when login with google fail
+    @param error the SignIn error object
+   */
+  handleSignInError = async error => {
+    if (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        this.showSignInError('User cancelled the login flow.');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        this.showSignInError('Sign in is in progress. ');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        await this.getGooglePlayServices();
+      } else {
+        this.showSignInError(JSON.stringify(error));
+      }
+    } else {
+      this.showSignInError(JSON.stringify(error));
+    }
+  };
+
+  /**
+  Checks if device has Google Play Services installed
+ */
+  getGooglePlayServices = async () => {
+    try {
+      await GoogleSignin.hasPlayServices({
+        showPlayServicesUpdateDialog: true,
+      });
+    } catch (error) {
+      this.showSignInError('Google play services are not available');
+    }
+  };
+
+  showSignInError = message => {
+    Alert.alert('Signin Error', message, [{text: 'OK'}], {
+      cancelable: false,
+    });
+  };
+
+  /**
+    Handle logout
+   */
+  logout = async () => {
+    const {t} = this.props;
+    Alert.alert(
+      t('auth:logout'),
+      t('auth:do you want to logout'),
+      [
+        {text: t('auth:cancle'), style: 'cancel'},
+        {
+          text: t('auth:ok'),
+          onPress: () => {
+            const {isSignedInGoogle} = this.state;
+            if (isSignedInGoogle) {
+              this.logoutGoogle();
+            } else {
+              this.logoutFaceBook();
+            }
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  };
+
+  logoutFaceBook = () => {
+    LoginManager.logOut(); // logout facebook
+    this.setState({
+      user: null,
+      isSignedIn: false,
+      isSignedInGoogle: false,
+    });
+  };
+
+  logoutGoogle = async () => {
+    try {
+      // await GoogleSignin.revokeAccess(); //Removes your application from the user authorized applications.
+      await GoogleSignin.signOut(); // logout google
+      this.setState({
+        user: null,
+        isSignedIn: false,
+        isSignedInGoogle: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
@@ -152,7 +255,7 @@ class Menu extends Component {
       divider,
     } = styles;
     const {t} = this.props;
-    const {user} = this.state;
+    const {user, isSignedIn} = this.state;
 
     let facebookButtonJSX = (
       <FontAwesome.Button
@@ -160,7 +263,7 @@ class Menu extends Component {
         style={button}
         backgroundColor="#3A63BF"
         onPress={this.loginFacebook}>
-        <Text style={loginButtonTitle}>Login with Facebook</Text>
+        <Text style={loginButtonTitle}>{t('auth:login with facebook')}</Text>
       </FontAwesome.Button>
     );
     let googleButtonJSX = (
@@ -169,29 +272,31 @@ class Menu extends Component {
         style={button}
         backgroundColor="#C94131"
         onPress={this.loginGoogle}>
-        <Text style={loginButtonTitle}>Login with Google</Text>
+        <Text style={loginButtonTitle}>{t('auth:login with google')}</Text>
       </FontAwesome.Button>
     );
     let logoutJSX = (
       <FontAwesome.Button
         name="sign-out"
-        backgroundColor="transparent"
+        backgroundColor="#FFF"
         color="#545659"
         onPress={this.logout}>
-        <Text style={menuText}>Logout</Text>
+        <Text style={menuText}>{t('auth:logout')}</Text>
       </FontAwesome.Button>
     );
 
-    facebookButtonJSX = !user ? facebookButtonJSX : null;
-    googleButtonJSX = !user ? googleButtonJSX : null;
-    logoutJSX = user ? logoutJSX : null;
+    facebookButtonJSX = !isSignedIn ? facebookButtonJSX : null;
+    googleButtonJSX = !isSignedIn ? googleButtonJSX : null;
+    logoutJSX = isSignedIn ? logoutJSX : null;
     var avatarUrl;
     if (user && user.photo) {
       avatarUrl = user.photo;
     } else if (user && user.picture) {
       avatarUrl = user.picture.data.url;
     }
-    const avatarJSX = <Image style={avatar} source={{uri: avatarUrl}} />;
+    const avatarJSX = (
+      <Avatar size="medium" rounded style={avatar} source={{uri: avatarUrl}} />
+    );
     const logoJSX = (
       <Image style={logoMenu} source={require('../../../images/LOGO.png')} />
     );
@@ -257,9 +362,10 @@ const styles = StyleSheet.create({
   },
   menuBanner: {
     flex: 2,
-    paddingVertical: 20,
+    paddingVertical: 40,
     paddingHorizontal: 30,
     justifyContent: 'center',
+    alignItems: 'center',
   },
   menuWrapper: {
     flex: 4,
@@ -287,14 +393,14 @@ const styles = StyleSheet.create({
   avatar: {
     width: 120,
     height: 120,
-    borderRadius: 120 / 2,
-    resizeMode: 'cover',
-    borderColor: '#FFF',
-    borderWidth: 2,
+    borderRadius: 60,
+    overflow: 'hidden',
   },
   text: {
     paddingTop: 3,
     color: '#575A63',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   ensignIcon: {
     flex: 1,
@@ -320,4 +426,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default withNamespaces(['language_setting'], {wait: true})(Menu);
+export default withNamespaces(['language_setting', 'auth'], {wait: true})(Menu);
